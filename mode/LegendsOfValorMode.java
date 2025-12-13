e59fc0730424368d2072bb13d1a5055c4ce3dd3f
akash <akash.sivanandan@pricelabs.co>
2025-12-13 18:08:38 -0500

    Wire state manager into LegendsOfValorMode

--- mode/LegendsOfValorMode.java
+++ mode/LegendsOfValorMode.java
@@ -1,11 +1,21 @@
 package mode;
 
 import engine.GameEngine;
+import state.ExplorationState;
+import state.GameContext;
+import state.StateManager;
 
 public class LegendsOfValorMode implements GameMode {
 
     @Override
     public void start() {
-        System.out.println("Legends of valor");
+        GameContext gameContext = new GameContext();
+        StateManager stateManager = new StateManager();
+
+        stateManager.changeState(new ExplorationState(gameContext));
+
+        while (gameContext.gameRunning){
+            stateManager.update();
+        }
     }
 }
