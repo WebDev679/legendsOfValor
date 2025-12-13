5d0361a87621b1c1cd9777e1e753f9e6b9617966
akash <akash.sivanandan@pricelabs.co>
2025-12-13 18:48:38 -0500

    Add checks to resolve hero action, pass to resolver in hero state

--- combat/ValorBattle.java
+++ combat/ValorBattle.java
@@ -3,23 +3,27 @@ import character.Hero;
 import character.Monster;
 import ai.MonsterAI;
 import java.util.List;
+import combat.action.*;
 
 public class ValorBattle {
     private final MonsterAI monsterAI = new MonsterAI();
 
-    public boolean execute(List<Hero> heroes, List <Monster> monsters) {
-        while (hasAliveHero(heroes) && hasAliveMonster(monsters)){
-            for (Hero hero: heroes){
-                if (!hero.isAlive()){
-                    continue;
-                }
+    public boolean execute(
+            List<Hero> heroes,
+            List<Monster> monsters,
+            HeroActionManager actionManager
+    ) {
 
-                Monster target = findMonsterInRange(hero, monsters);
-                if (target != null){
-                    ValorCombatExecutor.heroAttack(hero, target);
-                }
+        while (hasAliveHero(heroes) && hasAliveMonster(monsters)) {
+
+            for (Hero hero : heroes) {
+                if (!hero.isAlive()) continue;
+
+                HeroAction action = actionManager.nextAction(hero);
+                resolveHeroAction(hero, action, monsters);
             }
-            for  (Monster monster: monsters){
+
+            for (Monster monster : monsters) {
                 if (!monster.isAlive()){
                     continue;
                 }
@@ -31,6 +35,23 @@ public class ValorBattle {
             }
         }
         return hasAliveHero(heroes);
+    }
+
+    private void resolveHeroAction(
+            Hero hero,
+            HeroAction action,
+            List<Monster> monsters
+    ) {
+
+        if (action instanceof AttackAction) {
+            AttackAction attack = (AttackAction) action;
+            Monster target = attack.getTarget();
+
+            if (!target.isAlive()) return;
+            if (!ValorCombatRules.canAttack(hero, target)) return;
+
+            ValorCombatExecutor.heroAttack(hero, target);
+        }
     }
 
     private boolean hasAliveMonster(List<Monster> monsters) {
@@ -49,17 +70,5 @@ public class ValorBattle {
             }
         }
         return false;
-    }
-
-    private Monster findMonsterInRange(Hero h, List<Monster> monsters) {
-        for (Monster monster: monsters){
-            if (!monster.isAlive()){
-                continue;
-            }
-            if (ValorCombatRules.canAttack(h, monster)){
-                return monster;
-            }
-        }
-        return null;
     }
 }
--- state/BattleState.java
+++ state/BattleState.java
@@ -4,20 +4,25 @@ import combat.ValorBattle;
 public class BattleState implements GameState {
     private final GameContext context;
     private final ValorBattle battle = new ValorBattle();
+    private final StateManager stateManager;
 
-    public BattleState(GameContext context) {
+    public BattleState(GameContext context, StateManager stateManager) {
         this.context = context;
+        this.stateManager = stateManager;
     }
 
 
     @Override
     public void enter() {
         System.out.println("Entered battle!");
-        boolean won = battle.execute(context.heroes, context.monsters);
-
+        boolean won = battle.execute(context.heroes, context.monsters, context.actionManager);
         if (!won){
             context.gameRunning = false;
+            stateManager.changeState(null);
+            return;
         }
+        context.monsters.clear();
+        stateManager.changeState(new ExplorationState(context, stateManager));
     }
 
     @Override
