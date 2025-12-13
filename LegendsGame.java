f3fdb259bb65122e996b66a18acbe8cb1f795a9d
akash <akash.sivanandan@pricelabs.co>
2025-12-13 15:27:31 -0500

    Add wrapper so that user can choose between monsters vs heroes and legends of valor

--- LegendsGame.java
+++ LegendsGame.java
@@ -1,10 +1,43 @@
-// LegendsGame.java
-import engine.GameEngine;
+import mode.*;
+
+import java.util.Scanner;
 
 public class LegendsGame {
-    public static void main(String[] args) {
-        GameEngine engine = new GameEngine();
-        engine.start();
+    public static void main(String[] args){
+        GameMode mode = chooseMode();
+        if (mode == null){
+            return;
+        }
+        mode.start();
+    }
+
+    private static GameMode chooseMode(){
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Select a game mode:");
+            System.out.println("1. Monsters vs Heroes");
+            System.out.println("2. Legends of Valor");
+
+            String choice = input.nextLine().trim().toLowerCase();
+            if (choice.equals("q")){
+                return null;
+            }
+            int mode;
+            try{
+                mode = Integer.parseInt(choice);
+            } catch(NumberFormatException e){
+                System.out.println("Please enter a valid choice");
+                continue;
+            }
+
+            switch (mode){
+                case 1:
+                    return new MonstersAndHeroesMode();
+                case 2:
+                    return new LegendsOfValorMode();
+                default:
+                    System.out.println("Invalid input! Please try again");
+            }
+        }
     }
 }
-
