28885d485421c17d26c38036359e136671ae43c6
akash <akash.sivanandan@pricelabs.co>
2025-12-13 18:47:26 -0500

    Add dummy exploration state for testing

--- state/ExplorationState.java
+++ state/ExplorationState.java
@@ -1,10 +1,18 @@
 package state;
 
+import character.Hero;
+import util.DataLoader;
+
+import java.util.Random;
+
 public class ExplorationState implements GameState {
     private final GameContext context;
+    private final StateManager stateManager;
+    private final Random random = new Random();
 
-    public ExplorationState(GameContext context) {
+    public ExplorationState(GameContext context, StateManager stateManager) {
         this.context = context;
+        this.stateManager = stateManager;
     }
 
     @Override
@@ -14,6 +22,18 @@ public class ExplorationState implements GameState {
 
     @Override
     public void update() {
+        if (random.nextDouble() < 0.2){
+            System.out.println("Encountered a monster!");
+            int heroesCount = context.heroes.size();
+            int maxLevel = 1;
+            for (Hero hero : context.heroes){
+                maxLevel = Math.max(maxLevel, hero.getLevel());
+            }
+
+            context.monsters = DataLoader.generateRandomMonsters(heroesCount, maxLevel);
+            stateManager.changeState(new BattleState(context, stateManager));
+
+        }
     }
 
     @Override
