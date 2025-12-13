5d0a469a6a058d926fc85c8ba367c22851a342b8
akash <akash.sivanandan@pricelabs.co>
2025-12-13 15:28:09 -0500

    Wrap both games inside GameMode

---
+++ mode/LegendsOfValorMode.java
@@ -0,0 +1,11 @@
+package mode;
+
+import engine.GameEngine;
+
+public class LegendsOfValorMode implements GameMode {
+
+    @Override
+    public void start() {
+        System.out.println("Legends of valor");
+    }
+}
