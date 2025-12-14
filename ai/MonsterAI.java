d33ee22a14b1528ce4259fedb7edb75fb1975b4a
akash <akash.sivanandan@pricelabs.co>
2025-12-13 22:56:05 -0500

    Add javadoc dcomments for monsterai

--- ai/MonsterAI.java
+++ ai/MonsterAI.java
@@ -6,7 +6,25 @@ import combat.ValorCombatRules;
 import world.Position;
 import java.util.List;
 
+
+/**
+ * Controls autonomous behaviour for monsters during their turn
+ * <p>The MonsterAI follows a priority based model: </p>
+ * <ul>
+ *     <li>If an eligible hero is in range, attack</li>
+ *     <li>Otherwise, move forward toward nexus</li>
+ * </ul>
+ * <p>This class encapsulates all monster decision making logic
+ * and aloowes combat flow to be independent of AI strategy details</p>
+ */
 public class MonsterAI {
+    /**
+     * Executes a single turn for given monster
+     * <p>The monster will attempt to attack first valid alive hero
+     * within range. If none exist, monster advances forward by a space.</p>
+     * @param monster the monster taking its turn
+     * @param heroes the list of heroes currently ingame
+     */
     public void takeTurn(Monster monster, List<Hero> heroes){
         for (Hero hero: heroes){
             if (!hero.isAlive()){
@@ -20,6 +38,12 @@ public class MonsterAI {
         moveForward(monster);
     }
 
+    /**
+     * Moves monster forward one space towards heroes side
+     * <p>This method purely handles movement. Validation and lane boundaries are
+     * handled elsehwer in game logic</p>
+     * @param monster the monster to move
+     */
     private void moveForward(Monster monster) {
         Position initialPos = monster.getPosition();
         if (initialPos == null){
