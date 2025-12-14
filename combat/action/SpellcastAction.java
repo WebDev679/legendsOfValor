13f00a87541fb8b6d43b1ba456baef9f1889fa85
akash <akash.sivanandan@pricelabs.co>
2025-12-13 19:16:06 -0500

    Make spellcastaction also store attacker details

--- combat/action/SpellcastAction.java
+++ combat/action/SpellcastAction.java
@@ -1,14 +1,17 @@
 package combat.action;
+import character.Hero;
 import character.Monster;
 import item.Spell;
 
 public class SpellcastAction implements HeroAction{
     private final Spell spell;
     private final Monster target;
+    private final Hero hero;
 
-    public SpellcastAction(Spell spell, Monster target) {
+    public SpellcastAction(Hero hero, Spell spell, Monster target) {
         this.spell = spell;
         this.target = target;
+        this.hero = hero;
     }
 
     public Spell getSpell() {
@@ -17,5 +20,9 @@ public class SpellcastAction implements HeroAction{
 
     public Monster getTarget() {
         return target;
+    }
+
+    public Hero getHero() {
+        return hero;
     }
 }
