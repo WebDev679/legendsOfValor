71ecbee2aa6a8f070e01e5666b595d740ee56978
akash <akash.sivanandan@pricelabs.co>
2025-12-13 20:13:57 -0500

    Keep track of current round, and add helper functions for checking if there are any heroes / monsters alive

--- state/GameContext.java
+++ state/GameContext.java
@@ -12,6 +12,7 @@ public class GameContext {
     public GameMap map;
     public HeroActionManager actionManager;
 
+    public int round = 1;
     public boolean gameRunning = true;
 
     public boolean monsterReachedHeroNexus() {
@@ -22,6 +23,24 @@ public class GameContext {
 
     public boolean heroReachedEnemyNexus() {
         // TODO: implement logic later
+        return false;
+    }
+
+    public boolean hasAliveHeroes() {
+        for (Hero hero : heroes) {
+            if (hero.isAlive()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasAliveMonsters() {
+        for (Monster monster : monsters) {
+            if (monster.isAlive()) {
+                return true;
+            }
+        }
         return false;
     }
 }
