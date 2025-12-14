88e2a7654462c620a51ef59389b024493b0ba2dd
akash <akash.sivanandan@pricelabs.co>
2025-12-13 19:50:58 -0500

    Update ValorBattle as per spec.
    IMP: Each battle executes in parallel with the round, does not spawn a separate battle loop

--- combat/ValorBattle.java
+++ combat/ValorBattle.java
@@ -9,74 +9,54 @@ import util.PrintUtils;
 public class ValorBattle {
     private final MonsterAI monsterAI = new MonsterAI();
 
-    public boolean execute(
+    public void resolveRound(
             List<Hero> heroes,
             List<Monster> monsters,
             HeroActionManager actionManager
     ) {
-        int round = 1;
-        try{
-            while (hasAliveHero(heroes) && hasAliveMonster(monsters)) {
-                PrintUtils.pause(1000);
-                PrintUtils.clearScreen();
-                System.out.println("\n========= ROUND " + round + " =========");
-                printBattleState(heroes, monsters);
 
-                for (Hero hero : heroes) {
-                    if (!hero.isAlive()) continue;
+        PrintUtils.pause(500);
+        PrintUtils.clearScreen();
 
-                    HeroAction action = actionManager.nextAction(hero);
-                    resolveHeroAction(action, monsters);
-                }
-
-                System.out.println("\n======== Starting Monsters turn! ========");
-                for (Monster monster : monsters) {
-                    if (!monster.isAlive()){
-                        continue;
-                    }
-                    monsterAI.takeTurn(monster, heroes);
-                }
-
-                System.out.println("\n======== End of round regeneration ========");
-                for (Hero hero: heroes){
-                    hero.regenAfterRound();
-                }
-                round ++;
-            }
-        } catch (QuitBattleException e){
-            System.out.println("======== Player quit battle! ========");
-            return false;
-        }
-        System.out.println("\n======== Battle over! ========");
+        System.out.println("\n========= ROUND =========");
         printBattleState(heroes, monsters);
-        return hasAliveHero(heroes);
-    }
 
-    private void printBattleState(List<Hero> heroes, List<Monster> monsters) {
-        System.out.println("\nHeroes: ");
         for (Hero hero : heroes) {
-            System.out.printf(" %s | HP: %d/%d | MP: %d/%d%n",
-                    hero.getName(),
-                    hero.getHp(), hero.getMaxHp(),
-                    hero.getMp(), hero.getMaxMp()
-                    );
+            if (!hero.isAlive()) continue;
+
+            HeroAction action = actionManager.nextAction(hero);
+            resolveHeroAction(action, monsters);
         }
 
-        System.out.println("\nMonsters: ");
+        System.out.println("\n======== Monsters' Turn ========");
         for (Monster monster : monsters) {
-            System.out.printf(
-                    " %s | HP: %d/%d%n",
-                    monster.getName(),
-                    monster.getHp(),
-                    monster.getMaxHp()
-            );
+            if (!monster.isAlive()) continue;
+            monsterAI.takeTurn(monster, heroes);
+        }
+
+        System.out.println("\n======== End of Round Regen ========");
+        for (Hero hero : heroes) {
+            hero.regenAfterRound();
         }
     }
 
     private void resolveHeroAction(
             HeroAction action,
             List<Monster> monsters
-    ) throws QuitBattleException {
+    ) {
+
+        if (action == null) return;
+
+        if (action instanceof QuitAction) {
+            throw new RuntimeException("Player quit game");
+        }
+
+        if (action instanceof SkipAction) {
+            System.out.println(
+                    ((SkipAction) action).getHero().getName() + " skipped their turn."
+            );
+            return;
+        }
 
         if (action instanceof AttackAction) {
             AttackAction attack = (AttackAction) action;
@@ -98,38 +78,33 @@ public class ValorBattle {
             if (!target.isAlive()) return;
             if (!ValorCombatRules.canAttack(caster, target)) return;
 
-            ValorCombatExecutor.heroCastSpell(caster, target, spellcast.getSpell());
-            return;
+            ValorCombatExecutor.heroCastSpell(
+                    caster,
+                    target,
+                    spellcast.getSpell()
+            );
         }
-
-        if (action instanceof QuitAction){
-            throw new QuitBattleException();
-        }
-
-        if (action instanceof SkipAction){
-            System.out.println(((SkipAction) action).getHero().getName() + " skipped a turn!");
-            return;
-        }
-         if (action == null){
-             return;
-         }
     }
 
-    private boolean hasAliveMonster(List<Monster> monsters) {
-        for (Monster monster: monsters){
-            if (monster.isAlive()){
-                return true;
-            }
+    private void printBattleState(List<Hero> heroes, List<Monster> monsters) {
+        System.out.println("\nHeroes:");
+        for (Hero hero : heroes) {
+            System.out.printf(
+                    " %s | HP %d/%d | MP %d/%d%n",
+                    hero.getName(),
+                    hero.getHp(), hero.getMaxHp(),
+                    hero.getMp(), hero.getMaxMp()
+            );
         }
-        return false;
-    }
 
-    private boolean hasAliveHero(List<Hero> heroes) {
-        for (Hero hero: heroes){
-            if (hero.isAlive()){
-                return true;
-            }
+        System.out.println("\nMonsters:");
+        for (Monster monster : monsters) {
+            System.out.printf(
+                    " %s | HP %d/%d%n",
+                    monster.getName(),
+                    monster.getHp(),
+                    monster.getMaxHp()
+            );
         }
-        return false;
     }
 }
